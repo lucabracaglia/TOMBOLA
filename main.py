742c89e49b223898c6f4e7afb68671bbaee78d4f
luca <lucabracaglia9908@gmail.com>
2022-02-22 10:59:57 +0100

    fix error

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import sys
 parser = Utils.initialize_parser()
 n_giocatori=parser.g
 n_cartelle=parser.n
-if n_giocatori==None and n_cartelle==None:
+if n_giocatori==None or n_cartelle==None:
     print('Digita -h per vedere gli argomenti da inserire sulla linea di comando')
     sys.exit()
 if (Utils.numero_giocatori(n_giocatori)) and (Utils.confronta_lista_cartelle(n_giocatori,n_cartelle)) and (Utils.numero_cartelle(n_cartelle)):
