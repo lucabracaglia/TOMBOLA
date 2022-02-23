80a330cbd9d01c74b9e2247df3b68a8fde3f1810
luca <lucabracaglia9908@gmail.com>
2022-02-23 11:19:09 +0100

    Aggiunti commenti a tabellone

--- Tabellone.py
+++ Tabellone.py
@@ -1,5 +1,6 @@
 import numpy as np
-'''la classe tabellone conterrà 6 cartelle e tutti i numeri da 1 a 90 ordinati in maniera crescente.'''
+'''La classe tabellone conterrà 6 cartelle e tutti i numeri da 1 a 90 ordinati in maniera crescente.
+    Tuttavia per migliorare la visualizzazione del tabellone saranno utilizzate coppie di cartelle adiacenti'''
 class Tabellone():
 
     def __init__(self):
@@ -9,8 +10,8 @@ class Tabellone():
         self.cartellone_dx=[]
         self.cartellone_dx_sx=[]
 
-        '''il tabellone è composto da 6 cartelle 5X3, in cui ogni riga è composta da sequenze ordinate
-            le cartelle nel tabellone reale vangono disposte a coppie una accanto all' altra e pertanto si avranno 3 cartelle sul lato sinistro e 3 cartelle sul lato destro.
+        '''Il tabellone è composto da 6 cartelle 5X3, in cui ogni riga è composta da sequenze ordinate di numeri
+        Le cartelle nel tabellone reale vangono disposte a coppie una accanto all'altra e pertanto si avranno 3 cartelle sul lato sinistro e 3 cartelle sul lato destro.
         '''
 
         c=[]
@@ -103,21 +104,23 @@ class Tabellone():
             l=c[i]+85
             c17.append(l)
 
-        '''viene realizzate la prima cartella del tabellone che ha i segenti valori:
+        '''Viene realizzate la prima cartella del tabellone che ha i segenti valori:
         1 2 3 4 5
         11 12 13 14 15
         21 22 23 24 25
         '''
+        'Cartella in alto a sinistra'
         cartella_tabellone1=np.zeros((3,5))
-        cartella_tabellone1[0]=c
-        cartella_tabellone1[1]=c2
-        cartella_tabellone1[2]=c4
-
+        cartella_tabellone1[0]=c #1,2,3,4,5
+        cartella_tabellone1[1]=c2 #11,12,13,14,15
+        cartella_tabellone1[2]=c4 #21,22,23,24,25
+        'Analogamente si procede per le altre cartelle del tabellone'
+        'Cartella in alto a destra del tabellone'
         cartella_tabellone2=np.zeros((3,5))
         cartella_tabellone2[0]=c1
         cartella_tabellone2[1]=c3
         cartella_tabellone2[2]=c5 
-
+        'Si procede valutando la cartella centrale a sinistra del tabellone e così via per le altre'
         cartella_tabellone3=np.zeros((3,5))
         cartella_tabellone3[0]=c6
         cartella_tabellone3[1]=c8
@@ -138,7 +141,7 @@ class Tabellone():
         cartella_tabellone6[1]=c15
         cartella_tabellone6[2]=c17 
 
-        '''per permettere una visualizzazione migliore si affiancano le cartelle vicine.'''
+        '''Per permettere una visualizzazione migliore del tabellone si affiancano le cartelle adiacenti, in modo da formare 3 coppie di cartelle'''
         coppia1=np.zeros((3,10))
         coppia1[0]=c+c1
         coppia1[1]=c2+c3
@@ -154,30 +157,33 @@ class Tabellone():
         coppia3[1]=c14+c15
         coppia3[2]=c16+c17
 
-        '''si realizzano delle liste conteneti tutte le cartelle del cartellone per permettere agevolmente una varifica dei numeri estratti'''
+        '''Si realizzano delle liste contenenti tutte le cartelle del cartellone per permettere agevolmente una verifica dei numeri estratti.
+        Nel cartellone sx avremo le cartelle situate sulla sinistra del tabellone, lo stesso per quella di destra'''
+
         self.cartellone_sx.append(cartella_tabellone1)
         self.cartellone_dx.append(cartella_tabellone2)
         self.cartellone_sx.append(cartella_tabellone3)
         self.cartellone_dx.append(cartella_tabellone4)
         self.cartellone_sx.append(cartella_tabellone5)
         self.cartellone_dx.append(cartella_tabellone6)
-
+        
+        '''Cartellone_dx_sx contiene tutte le cartelle del tabellone'''
         self.cartellone_dx_sx=self.cartellone_sx+self.cartellone_dx
 
-        '''si realizzano tre liste contenteni ciascuna 2 cartelle per permettere una visualizzazione migliore'''
+        '''Si realizzano tre liste contententi ciascuna 2 cartelle per permettere una visualizzazione migliore'''
         self.cartellone.append(coppia1)
         self.cartellone.append(coppia2)
         self.cartellone.append(coppia3)
 
-    '''metodo che permette la visualizzazione di un singolo elemento del tabellone noti gli indici di posizione
+    '''Metodo che permette la visualizzazione di un singolo elemento del tabellone noti gli indici di posizione
         NB: poichè risulta che il cartellone sia una lista di matrici 3X5 vanno specificati tre indici:
-        - pos_cartella indica la matrice a cui ci riferiamo 
-        - index_riga e index_colonna fanno riferimento agli indici di posizione all' interno della matrice.
+        - pos_cartella indica la cartella a cui ci riferiamo 
+        - index_riga e index_colonna fanno riferimento agli indici di posizione all'interno della matrice specificata.
     '''
     def visualizza_elemento_tabellone(self,pos_cartella,index_riga,index_colonna):
         return self.cartellone[pos_cartella][index_riga][index_colonna]
 
-    '''effettua la stampa del tabellone'''   
+    '''Effettua la stampa del tabellone'''   
     def visualizza_tabellone(self):
         for p in range(3):
             for r in range(3):
@@ -193,16 +199,19 @@ class Tabellone():
             
             pass
         
-
+    '''Dato in ingresso l'indice i della cartella nel tab_sx viene restituita tale cartella'''
     def singola_cartella_tab_sx(self,i):
         return self.cartellone_sx[i]
-    
+
+    '''Analogo al precedente per le cartelle di dx del tabellone'''
     def singola_cartella_tab_dx(self,i):
         return self.cartellone_dx[i]
-    
+
+    '''Dato in ingresso l'indice della cartella iesima , questa volta i=(0,5) mi restituisce la cartella associata a quell'indice'''
     def singola_cartella_tab_dx_sx(self,i):
         return self.cartellone_dx_sx[i]
 
+    '''Dato in ingresso un certo numero, mi viene restituito l'indice k della cartella in cui trovo n e gli indici di posizione nella matrice'''
     def pos_numero_estratto(self,n):
         for i in range(3):
             for j in range(5):
@@ -210,9 +219,13 @@ class Tabellone():
                     if self.singola_cartella_tab_dx_sx(k)[i][j]==n:
                         return k,i,j
 
+    '''Dati in ingresso gli stessi parametri che erano in uscita per pos numero estratto, sostitusico -1 nella posizione individuata dagli indici i,j nella cartella k'''
     def sostituisci_numero(self, k, i, j):
         self.singola_cartella_tab_dx_sx(k)[i][j]=-1
 
+    
+    '''Metodo analogo a pos numero estratto, solamente che mi viene restituita la kesima cartella con k=(0,1,2), avendo considerato per self.cartellone le coppie di cartelle, e degli indici i,j
+    diversi da quelli ottenuti per pos numero estratto'''
     def pos_numero_estratto_tabellone(self,n):
         for i in range(3):
             for j in range(10):
@@ -220,9 +233,12 @@ class Tabellone():
                     if self.cartellone[k][i][j]==n:
                         return k,i,j
     
+    '''Dati in ingresso k,i,j sostituisco -1 al posto del numero presente negli indici specificati'''
     def sostituisci_numero_tabellone(self,k,i,j):
         self.cartellone[k][i][j]=-1
 
+    '''Dati in ingresso la cartella k, con k=(0,6), e la riga iesima, vado a verificare quante volte compare -1 per ognmi singola riga.
+    Tale metodo sarà utilizzato per verificare le vincite del tabellone'''
     def conta_elementi_estratti_tabellone(self,k,i):
         c=0
         for j in range(5):
