23fda66b32e90d677a7972d17136172b53f533c7
luca <lucabracaglia9908@gmail.com>
2022-02-23 10:46:56 +0100

    Piccole modifiche

--- Utils.py
+++ Utils.py
@@ -15,7 +15,7 @@ def initialize_parser():
 
 def numero_giocatori(numero_giocatori):
     if numero_giocatori <= 1:
-        print('il numero di giocatori deve essere almeno maggiore di 1')
+        print('Il numero di giocatori deve essere almeno maggiore di 1')
         sys.exit()
     else:
         return True
@@ -27,32 +27,26 @@ def confronta_lista_cartelle(numero_giocatori, lista_cartelle):
         return True
     
     else:
-        print('il numero di cartelle deve corrispondere al numero di giocatori')
+        print('Il numero di cartelle deve corrispondere al numero di giocatori')
         exit()  
 
 '''verifica che ogni giocatore abbia un numero congruo di cartelle (almeno 1, massimo 6)'''
 def numero_cartelle(lista_cartelle):
-    contatore=0
     for i in lista_cartelle:
         if i<1:
-            contatore=contatore +1
-        elif i >=7:
-            contatore=contatore +1
-        else:
-            pass
-
-    if contatore!=0:
-        print('ogni giocatore deve disporre almeno di una cartella e un massimo di 6 cartelle')
-        return False
-    else:
-        return True
+            print('Ogni giocatore deve avere una o più cartelle')
+            sys.exit() 
+        elif i>7:
+            print('Ogni singolo giocatore non può avere più di 7 cartelle')
+            sys.exit() 
+    return lista_cartelle
 
 '''si estrae un numero casual finchè no se ne estrae uno che non è mai stato estratto'''
 def estrazione(estratti):
     
     numero_estratto = random.randint(1, 90)
     if numero_estratto not in estratti:    
-        print("il numero estratto è:", numero_estratto)
+        print("Il numero estratto è:", numero_estratto)
         estratti.append(numero_estratto)
     else: 
         numero_estratto = estrazione(estratti)
